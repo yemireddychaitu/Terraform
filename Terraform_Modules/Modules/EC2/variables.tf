28b79279ceffd181f3358393edf381dbc7b4b5b3
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-07-15 09:40:13 +0530

    Create variables.tf

---
+++ Terraform_Modules/Modules/EC2/variables.tf
@@ -0,0 +1,27 @@
+variable "project_id_value" {
+    description = "value for the project"
+}
+
+variable "name_of_instance" {
+    description = "value for the instance"
+}
+
+variable "machine_type_value" {
+    description = "value for the machine type"
+}
+
+variable "zone_value" {
+    description = "value for the zone"
+}
+
+variable "image_value" {
+    description = "value for the image"
+}
+
+variable "network_value" {
+    description = "value for the VPC network"
+}
+
+variable "subnet_value" {
+    description = "value for the subnet name"
+}
