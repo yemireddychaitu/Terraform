9cbf08a36ff66d73ab4bdf6db4814c777ffed51e
yemireddychaitu <67100603+yemireddychaitu@users.noreply.github.com>
2025-07-15 09:34:10 +0530

    Create variables.tf

---
+++ Terraform_Modules/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "GCP region"
+  type        = string
+  default     = "cts13-yemireddyc"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
